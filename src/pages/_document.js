82662203c32c7da4fa3398a2ae70a33ad6ce6256
alecap92 <alejandro.cabrejo@gmail.com>
2023-01-26 05:33:58 -0500

    fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -14,11 +15,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script
+        <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
           crossorigin="anonymous"
-        ></script>
+        ></Script>
       </body>
     </Html>
   );
