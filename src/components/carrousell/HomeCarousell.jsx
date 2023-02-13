5e7350e34b85e2011b46507a33875e471245f272
alecap92 <alejandro.cabrejo@gmail.com>
2023-02-13 12:28:37 -0500

    upd img

--- src/components/carrousell/HomeCarousell.jsx
+++ src/components/carrousell/HomeCarousell.jsx
@@ -6,6 +6,128 @@ import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import Link from "next/link.js";
 
 const HomeCarousell = ({ forms, userInfo }) => {
+  const products = [
+    {
+      name: "bakery",
+      img: "/img/imgHome/1.png",
+    },
+    {
+      name: "cocoa",
+      img: "/img/imgHome/2.png",
+    },
+    {
+      name: "coffee",
+      img: "/img/imgHome/3.png",
+    },
+    {
+      name: "confitery",
+      img: "/img/imgHome/4.png",
+    },
+    {
+      name: "drinks",
+      img: "/img/imgHome/5.png",
+    },
+    {
+      name: "energySnacks",
+      img: "/img/imgHome/6.png",
+    },
+    {
+      name: "flourMixes",
+      img: "/img/imgHome/7.png",
+    },
+    {
+      name: "freshFruits",
+      img: "/img/imgHome/8.png",
+    },
+    {
+      name: "HoneyJamAndSpreads",
+      img: "/img/imgHome/9.png",
+    },
+    {
+      name: "MeatAndLivestock",
+      img: "/img/imgHome/10.png",
+    },
+    {
+      name: "OilVinegar",
+      img: "/img/imgHome/11.png",
+    },
+    {
+      name: "SaucesAndDressings",
+      img: "/img/imgHome/12.png",
+    },
+    {
+      name: "superfoods",
+      img: "/img/imgHome/13.png",
+    },
+    {
+      name: "SugarCaneTea",
+      img: "/img/imgHome/14.png",
+    },
+    {
+      name: "flowers",
+      img: "/img/imgHome/15.png",
+    },
+    {
+      name: "rosesflowers",
+      img: "/img/imgHome/16.png",
+    },
+    {
+      name: "BathAndShower",
+      img: "/img/imgHome/17.png",
+    },
+    {
+      name: "bodyCare",
+      img: "/img/imgHome/18.png",
+    },
+    {
+      name: "facialCare",
+      img: "/img/imgHome/19.png",
+    },
+    {
+      name: "hairCare",
+      img: "/img/imgHome/20.png",
+    },
+    {
+      name: "aromaHome",
+      img: "/img/imgHome/21.png",
+    },
+    {
+      name: "horseSaddles",
+      img: "/img/imgHome/22.png",
+    },
+    {
+      name: "feedstuff",
+      img: "/img/imgHome/24.png",
+    },
+    {
+      name: "clothing",
+      img: "/img/imgHome/25.png",
+    },
+    {
+      name: "compressionGirdle",
+      img: "/img/imgHome/26.png",
+    },
+    {
+      name: "hats",
+      img: "/img/imgHome/27.png",
+    },
+    {
+      name: "Jewelery",
+      img: "/img/imgHome/28.png",
+    },
+    {
+      name: "swimsuits",
+      img: "/img/imgHome/29.png",
+    },
+    {
+      name: "towels",
+      img: "/img/imgHome/30.png",
+    },
+    {
+      name: "womenShapeware",
+      img: "/img/imgHome/31.png",
+    },
+  ];
   let scrollAmount = 0;
 
   const sideScroll = (element, speed, distance, step) => {
@@ -30,21 +152,11 @@ const HomeCarousell = ({ forms, userInfo }) => {
         </CarouselArrow>
 
         <ContenedorCarrouselll className="contenedorCarrouselll" id="contenedorCarrouselll">
-          <Link href="/productos/bakery" style={{ margin: "0px 20px" }}>
-            <img src="\img\Inicio\1.png" alt="" width={300} height={160} />
-          </Link>
-          <Link href="/productos/bakery" style={{ margin: "0px 20px" }}>
-            <img src="\img\Inicio\2.png" alt="" width={300} height={160} />
-          </Link>
-          <Link href="/productos/bakery" style={{ margin: "0px 20px" }}>
-            <img src="\img\Inicio\3.png" alt="" width={300} height={160} />
-          </Link>
-          <Link href="/productos/bakery" style={{ margin: "0px 20px" }}>
-            <img src="\img\Inicio\4.png" alt="" width={300} height={160} />
-          </Link>
-          <Link href="/productos/bakery" style={{ margin: "0px 20px" }}>
-            <img src="\img\Inicio\5.png" alt="" width={300} height={160} />
-          </Link>
+          {products.map((item, index) => (
+            <Link key={index} href={"/productos/" + item.name} style={{ margin: "0px 20px" }}>
+              <img src={item.img} alt={item.name} width={300} height={160} />
+            </Link>
+          ))}
         </ContenedorCarrouselll>
 
         <CarouselArrow
